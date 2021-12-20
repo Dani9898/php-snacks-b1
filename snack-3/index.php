3bb0ea3dae62320916fe31a2ccfe01c6c3fcf829
Dani9898 <danieleoliva98@gmail.com>
2021-12-20 17:34:07 +0100

    feat: print date with relatives posts (snack 3)

---
+++ snack-3/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 3</title>
+</head>
+<body>
+    
+<?php
+
+    $posts = [
+
+        '10/01/2019' => [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
+        ],
+        '10/02/2019' => [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+            ]
+        ],
+        '15/05/2019' => [
+            [
+                'title' => 'Post 4',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 4'
+            ],
+            [
+                'title' => 'Post 5',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 5'
+            ],
+            [
+                'title' => 'Post 6',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 6'
+            ]
+        ],
+    ];
+
+
+?>
+
+    <div>
+
+        <?php
+            $keys = array_keys($posts);
+            
+            for ($i = 0; $i < count($posts); $i++) {
+
+                echo $keys[$i] . ": <br>";
+
+                for ($j = 0; $j < count($posts[$keys[$i]]); $j++) {
+                    echo $posts[$keys[$i]][$j]["title"] . "  " . $posts[$keys[$i]][$j]["author"]. "  " . $posts[$keys[$i]][$j]["text"] . "<br>";
+                } 
+            }
+
+        
+
+        ?>
+
+    </div>
+
+</body>
+</html>
